65c6a8eb7ec3ce21f0044fc751db2e5ba3482bf9
edli_01 <163815216+MengJunLi9999@users.noreply.github.com>
2024-12-25 17:14:34 +0800

    update the LICENSE.

--- src/main/java/org/edli01/Main.java
+++ src/main/java/org/edli01/Main.java
@@ -2,6 +2,6 @@ package org.edli01;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.printf("Welcome to here !");
+    System.out.printf("Welcome to here!");
   }
 }
